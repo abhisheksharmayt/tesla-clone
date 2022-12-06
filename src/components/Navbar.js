08d911d45d403a379e5926438580edbdeb06f515
Abhishek Sharma <ohsharmaabhi03@gmail.com>
2022-12-06 05:41:33 +0530

    fix: cart counter

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -45,7 +45,7 @@ const Navbar = () => {
             </div>
             <div className="nav-shop pr-10 flex">
                 <Link className='relative' to='/cart'>
-                    <div className='absolute right-0 top-1 text-center rounded-full h-4 w-4 bg-red-500 text-white text-xs'>
+                    <div className='absolute -right-1 text-center rounded-full h-4 w-4 bg-red-500 text-white text-xs'>
                         {cart.length}
                     </div>
                     <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 640 512" className="h-8 w-8 " height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M423.3 440.7c0 25.3-20.3 45.6-45.6 45.6s-45.8-20.3-45.8-45.6 20.6-45.8 45.8-45.8c25.4 0 45.6 20.5 45.6 45.8zm-253.9-45.8c-25.3 0-45.6 20.6-45.6 45.8s20.3 45.6 45.6 45.6 45.8-20.3 45.8-45.6-20.5-45.8-45.8-45.8zm291.7-270C158.9 124.9 81.9 112.1 0 25.7c34.4 51.7 53.3 148.9 373.1 144.2 333.3-5 130 86.1 70.8 188.9 186.7-166.7 319.4-233.9 17.2-233.9z"></path></svg>
